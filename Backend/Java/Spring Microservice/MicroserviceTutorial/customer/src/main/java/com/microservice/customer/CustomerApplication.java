1648554a404573632efc0426bd17451d1eeef379
Anjani Kumar Ray <kumar.anjaniray@gmail.com>
2024-04-29 12:24:04 +0530

    deployed microservices to k8s

--- Backend/Java/Spring Microservice/MicroserviceTutorial/customer/src/main/java/com/microservice/customer/CustomerApplication.java
+++ Backend/Java/Spring Microservice/MicroserviceTutorial/customer/src/main/java/com/microservice/customer/CustomerApplication.java
@@ -4,11 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {"com.microservice.customer","com.microservice.amqp"}
 )
 @EnableFeignClients("com.microservice.client")
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- Backend/Java/Spring Microservice/MicroserviceTutorial/notification/src/main/java/com/microservice/notification/NotificationApplication.java
+++ Backend/Java/Spring Microservice/MicroserviceTutorial/notification/src/main/java/com/microservice/notification/NotificationApplication.java
@@ -2,10 +2,15 @@ package com.microservice.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {"com.microservice.notification","com.microservice.amqp"}
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
